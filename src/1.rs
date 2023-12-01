2d1a17f6034c33bfdcc826c3d51fc944846585d3
Ivan Galakhov <igalakhov.nyc@gmail.com>
2023-12-01 18:30:19 -0500

    slight refactor

--- src/1.rs
+++ src/1.rs
@@ -41,17 +41,11 @@ impl Problem for Problem1 {
                 .iter()
                 .map(|line| {
                     match vec![
-                        ("one", 1),
-                        ("two", 2),
-                        ("three", 3),
-                        ("four", 4),
-                        ("five", 5),
-                        ("six", 6),
-                        ("seven", 7),
-                        ("eight", 8),
-                        ("nine", 9),
+                        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
                     ]
                     .into_iter()
+                    .map(str::to_string)
+                    .zip(1..10)
                     .map(|(i, n)| (i.to_string(), n))
                     .chain((1..10).map(|i| (format!("{}", i), i)))
                     .map(|(word, value)| {
