203bf3b336503329f841c8624aae0946f1144a8f
Ivan Galakhov <igalakhov.nyc@gmail.com>
2023-12-09 09:00:54 -0500

    catch up through day 9

---
+++ src/8.rs
@@ -0,0 +1,78 @@
+use aoc2023::{run_problem, Problem};
+use num;
+use regex::Regex;
+use std::{collections::HashMap, fmt::Display};
+
+struct Problem8 {
+    path: String,
+    graph: HashMap<String, (String, String)>,
+}
+
+impl Problem for Problem8 {
+    fn solve<F1, F2>(&self, report_first: F1, report_second: F2)
+    where
+        F1: FnOnce(&dyn Display) -> (),
+        F2: FnOnce(&dyn Display) -> (),
+    {
+        {
+            let mut steps = 0;
+            let mut cur = "AAA";
+            while cur != "ZZZ" {
+                let (left, right) = self.graph.get(cur).unwrap();
+                if self.path.as_bytes()[steps % self.path.len()] == b'R' {
+                    cur = right;
+                } else {
+                    cur = left;
+                }
+                steps += 1;
+            }
+
+            report_first(&steps);
+        }
+        {
+            let mut part2 = 1;
+
+            for (start, _) in &self.graph {
+                if start.as_bytes()[2] != b'A' {
+                    continue;
+                }
+
+                let mut steps = 0;
+                let mut cur = start;
+
+                while cur.as_bytes()[2] != b'Z' {
+                    let (left, right) = self.graph.get(cur).unwrap();
+                    if self.path.as_bytes()[steps % self.path.len()] == b'R' {
+                        cur = right;
+                    } else {
+                        cur = left;
+                    }
+                    steps += 1;
+                }
+
+                part2 = num::integer::lcm(part2, steps);
+            }
+
+            report_second(&part2);
+        }
+    }
+
+    fn parse(lines: Vec<String>) -> Self {
+        let re = Regex::new(r"([A-Z]{3}) = \(([A-Z]{3}), ([A-Z]{3})\)").unwrap();
+
+        Self {
+            path: lines[0].clone(),
+            graph: lines[2..]
+                .into_iter()
+                .map(|m| re.captures(m).unwrap().extract())
+                .map(|(_, [name, left, right])| {
+                    (name.to_string(), (left.to_string(), right.to_string()))
+                })
+                .collect::<HashMap<_, _>>(),
+        }
+    }
+}
+
+fn main() {
+    run_problem::<Problem8>("inputs/8.txt");
+}
---
+++ src/9.rs
@@ -0,0 +1,75 @@
+use aoc2023::{run_problem, Problem};
+use itertools::Itertools;
+use std::fmt::Display;
+
+struct Problem9 {
+    sequences: Vec<Vec<i64>>,
+}
+
+fn get_seqs(init: &Vec<i64>) -> Vec<Vec<i64>> {
+    let mut seqs = vec![init.clone()];
+
+    while !seqs.last().unwrap().iter().all(|x| x == &0) {
+        seqs.push(
+            seqs.last()
+                .unwrap()
+                .clone()
+                .into_iter()
+                .tuple_windows()
+                .map(|(x, y)| y - x)
+                .collect(),
+        )
+    }
+
+    seqs
+}
+
+fn predict_next(seq: &Vec<i64>) -> i64 {
+    get_seqs(seq).iter().map(|s| s.last().unwrap()).sum::<i64>()
+}
+
+fn predict_prev(seq: &Vec<i64>) -> i64 {
+    get_seqs(seq)
+        .into_iter()
+        .map(|s| *s.first().unwrap())
+        .enumerate()
+        .map(|(idx, num)| if idx % 2 == 0 { num } else { -num })
+        .sum::<i64>()
+}
+
+impl Problem for Problem9 {
+    fn solve<F1, F2>(&self, report_first: F1, report_second: F2)
+    where
+        F1: FnOnce(&dyn Display) -> (),
+        F2: FnOnce(&dyn Display) -> (),
+    {
+        report_first(
+            &self
+                .sequences
+                .iter()
+                .map(|seq| predict_next(seq))
+                .sum::<i64>(),
+        );
+
+        report_second(
+            &self
+                .sequences
+                .iter()
+                .map(|seq| predict_prev(seq))
+                .sum::<i64>(),
+        );
+    }
+
+    fn parse(lines: Vec<String>) -> Self {
+        Self {
+            sequences: lines
+                .into_iter()
+                .map(|line| line.split(" ").map(|s| s.parse::<i64>().unwrap()).collect())
+                .collect(),
+        }
+    }
+}
+
+fn main() {
+    run_problem::<Problem9>("inputs/9.txt");
+}
