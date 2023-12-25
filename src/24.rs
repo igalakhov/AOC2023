ec825e28bf7c5ead89d9549d8bae3135f09d680b
Ivan Galakhov <igalakhov.nyc@gmail.com>
2023-12-25 15:30:41 -0500

    day 24 finally

--- src/24.rs
+++ src/24.rs
@@ -1,7 +1,5 @@
 use aoc2023::{run_problem, Problem};
-use geo_types::{Line, Point};
 use itertools::Itertools;
-use line_intersection::LineInterval;
 use std::fmt::Display;
 
 struct Problem24 {
@@ -32,6 +30,76 @@ fn intersect_2d(
     };
 }
 
+fn determinant(matrix: Vec<Vec<f64>>) -> f64 {
+    if matrix.len() == 1 {
+        return matrix[0][0];
+    }
+
+    let mut ret = 0.0;
+
+    for j in 0..matrix.len() {
+        let subdet = matrix[0][j].clone()
+            * determinant(
+                matrix[1..]
+                    .into_iter()
+                    .map(|v| {
+                        v.clone()
+                            .iter()
+                            .enumerate()
+                            .filter(|(idx, _)| idx != &j)
+                            .map(|t| t.1.clone())
+                            .collect_vec()
+                    })
+                    .collect_vec(),
+            );
+
+        if j % 2 == 0 {
+            ret += subdet;
+        } else {
+            ret -= subdet;
+        }
+    }
+    ret
+}
+
+fn make_problem(
+    ((x1, y1, z1), (vx1, vy1, vz1)): ((f64, f64, f64), (f64, f64, f64)),
+    ((x2, y2, z2), (vx2, vy2, vz2)): ((f64, f64, f64), (f64, f64, f64)),
+    ((x3, y3, z3), (vx3, vy3, vz3)): ((f64, f64, f64), (f64, f64, f64)),
+) -> (Vec<Vec<f64>>, Vec<f64>) {
+    (
+        vec![
+            vec![0.0, vz1 - vz2, -vy1 + vy2, 0.0, -z1 + z2, y1 - y2],
+            vec![-vz1 + vz2, 0.0, vx1 - vx2, z1 - z2, 0.0, -x1 + x2],
+            vec![vy1 - vy2, -vx1 + vx2, 0.0, -y1 + y2, x1 - x2, 0.0],
+            vec![0.0, vz2 - vz3, -vy2 + vy3, 0.0, -z2 + z3, y2 - y3],
+            vec![-vz2 + vz3, 0.0, vx2 - vx3, z2 - z3, 0.0, -x2 + x3],
+            vec![vy2 - vy3, -vx2 + vx3, 0.0, -y2 + y3, x2 - x3, 0.0],
+        ],
+        vec![
+            -vy1 * z1 + vy2 * z2 + vz1 * y1 - vz2 * y2,
+            vx1 * z1 - vx2 * z2 - vz1 * x1 + vz2 * x2,
+            -vx1 * y1 + vx2 * y2 + vy1 * x1 - vy2 * x2,
+            -vy2 * z2 + vy3 * z3 + vz2 * y2 - vz3 * y3,
+            vx2 * z2 - vx3 * z3 - vz2 * x2 + vz3 * x3,
+            -vx2 * y2 + vx3 * y3 + vy2 * x2 - vy3 * x3,
+        ],
+    )
+}
+
+fn solve_cramer(m: Vec<Vec<f64>>, b: Vec<f64>) -> Vec<f64> {
+    let d = determinant(m.clone());
+    (0..m.len())
+        .map(|j| {
+            let mut col = m.clone();
+            (0..m.len()).for_each(|i| {
+                col[i][j] = b[i];
+            });
+            determinant(col) / d
+        })
+        .collect_vec()
+}
+
 impl Problem for Problem24 {
     fn solve<F1, F2>(&self, report_first: F1, report_second: F2) -> ()
     where
@@ -49,6 +117,12 @@ impl Problem for Problem24 {
                 })
                 .count(),
         );
+
+        let (m, b) = make_problem(self.stones[0], self.stones[1], self.stones[2]);
+
+        let sol = solve_cramer(m, b);
+
+        report_second(&sol.iter().take(3).sum::<f64>());
     }
 
     fn parse(lines: Vec<String>) -> Self {
